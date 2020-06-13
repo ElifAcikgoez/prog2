90c604a90fa8a1efb6cbf8823909b50e35039f62
ElifAcikgoez <elifnuracikgoez@gmail.com>
2020-06-13 16:07:41 +0200

    aufgabe 4 fertig

--- SoSe2020/src/gui/ButtonGroupDemo.java
+++ SoSe2020/src/gui/ButtonGroupDemo.java
@@ -1,16 +1,8 @@
 package gui;
 
-import java.awt.BorderLayout;
+import java.awt.*;
+import javax.swing.*;
 
-import java.awt.Font;
-import java.awt.GridLayout;
-
-import javax.swing.ButtonGroup;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 public class ButtonGroupDemo extends JFrame
@@ -29,39 +21,22 @@ public class ButtonGroupDemo extends JFrame
 		this.getContentPane().setLayout(new GridLayout(8,1));
 		
 		JPanel p1 = p1init();
-		JPanel p2 = p2init();
-		//JPanel p3 = p3init();
+	
+		
 		
 		
 	}
 
-	
-
-//	private JPanel p3init()
-	//{
-		//JPanel [] p3 = new JPanel[8];
-		/*/
-		JCheckBox one , two , three;
-		one = new JCheckBox("");
-		two = new JCheckBox("");
-		three = new JCheckBox("");
-		p3.add(one);
-		p3.add(two);
-		p3.add(three);
-
-		//this.getContentPane().add(p3);/*/
-		
-		
-		
-		
-		//return p3;
-//	}
-
-
-
-	private JPanel p2init()
+	private JPanel p1init()
 	{
+		JPanel p1 = new JPanel();
 		JPanel p2 = new JPanel();
+		JPanel p3 = new JPanel();
+		JPanel p4 = new JPanel();
+		JPanel p5 = new JPanel();
+		JPanel p6 = new JPanel();
+		
+		//Radiobutton
 		JRadioButton eins = new JRadioButton("");
 		JRadioButton zwei = new JRadioButton("");
 		JRadioButton drei = new JRadioButton("");
@@ -71,42 +46,80 @@ public class ButtonGroupDemo extends JFrame
 		gruppe.add(zwei);
 		gruppe.add(drei);
 		
-		//eins.setLayout(new FlowLayout(FlowLayout.CENTER));
-		p2.add(eins);
-		p2.add(zwei);
-		p2.add(drei);
 		
 		
+		//ungrupperte JRadioButton
+		JRadioButton ungr1 = new JRadioButton("");
+		JRadioButton ungr2 = new JRadioButton("");
+		JRadioButton ungr3 = new JRadioButton("");
+		ungr1.setSelected(true);
+		
+		
+		
+		//JCheckBox
 		JCheckBox one , two , three;
 		one = new JCheckBox("");
 		two = new JCheckBox("");
 		three = new JCheckBox("");
-		p2.add(one);
-		p2.add(two);
-		p2.add(three);
+		ButtonGroup gruppe2 = new ButtonGroup();
+		one.setSelected(true);
+		gruppe2.add(one);
+		gruppe2.add(two);
+		gruppe2.add(three);
+		
+		//ungrupperte JCheckBox
+		JCheckBox ungrj1 , ungrj2 , ungrj3;
+		ungrj1 = new JCheckBox("");
+		ungrj2 = new JCheckBox("");
+		ungrj3 = new JCheckBox("");
+		ungrj3.setSelected(true);
+		
 		
 	
-		this.getContentPane().add(p2);
+		//Label
+		JLabel label = new JLabel("Gruppiert: ");
 		
-		return p2;
-	}
-
-	private JPanel p1init()
-	{  setLayout(new BorderLayout());
-		JPanel p1 = new JPanel();
-		//p1.setLayout(new BorderLayout());
-		JLabel label = new JLabel("Gruppiert : ");
+		//label.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
 		label.setFont(new Font(Font.SANS_SERIF,Font.BOLD, 18));
-		//setLayout(new BorderLayout());
-	
-		//this.getContentPane().add(p1);
-		this.getContentPane().add(label,BorderLayout.NORTH );
 		
+		JLabel label2 = new JLabel("Nicht Gruppiert: ");
+		label2.setFont(new Font(Font.SANS_SERIF,Font.BOLD, 18));
+		
+		
+		p1.add(label);
+		p2.add(eins);
+		p2.add(zwei);
+		p2.add(drei);
+		p3.add(one);
+		p3.add(two);
+		p3.add(three);
+		p4.add(label2);
+		p5.add(ungr1);
+		p5.add(ungr2);
+		p5.add(ungr3);
+		p6.add(ungrj1);
+		p6.add(ungrj2);
+		p6.add(ungrj3);
+		
+		
+		this.getContentPane().add(p1);
+		this.getContentPane().add(p2);
+		this.getContentPane().add(p3);
+		this.getContentPane().add(p4);
+		this.getContentPane().add(p5);
+		this.getContentPane().add(p6);
 		
 		return p1;
 	}
+
 	
-	
+	/*/Was fällt bei beim Wechseln der Auswahl auf?
+	 * bei den grupperten Buttons kann man nur eins auswählen , also nur 
+	 * eine der drei buttons kann ausgewählt sein. Bei den ungrupperten
+	 * Buttons kann man alle 3 als ausgewählt kennzeichnen , also 
+	 * mehrere als nur eine auswahl.
+	 */
 	
 
 }
